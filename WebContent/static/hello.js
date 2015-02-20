30cef82a2e02ee10c9658506d1d2227d02120fc2
Sven Hasselbach <github@hasselba.ch>
2015-02-20 17:30:03 +0100

    Repaired AngularJS example

--- WebContent/static/hello.js
+++ WebContent/static/hello.js
@@ -1,5 +1,9 @@
 function Hello($scope, $http) {
+	
+	// add a valid token to the headers
 	$http.defaults.headers.common['X-AUTH-TOKEN'] = 'MTQyNDI2OTE0NDgxNCFUVGVzdFVzZXIwMQ==!HyC1mnvvdaneLaW0Wn48kZ1MaTrdowr1e4nWBRWRX8Y=';
+
+	// load the data
     $http.get('http://localhost/token/validate/').
         success(function(data) {
             $scope.greeting = data;
